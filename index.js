318caa2ca376a077103465a839e8f052378d5c93
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-13 16:12:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+const ptext = document.getElementById('text');
+const changeText =()=> ptext.innerText=" This is really cool!"
+ptext.addEventListener('DOMContentLoaded',changeText);
+
